7002328d7f64219e6fa3c1c89e041b64ba9bfc3c
Manavkotecha <146665096+Manavkotecha@users.noreply.github.com>
2025-02-11 22:08:48 +0530

    Add files via upload

---
+++ lib/spalshScreen.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:matrimony/dashboard.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Dashboard()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFB24592),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/heart.png',
+              height: 200,
+              width: 200,
+            ),
+
+            // App Name or Tagline
+            const SizedBox(height: 20),
+            const Text(
+              'Forever Together',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils.dart
@@ -0,0 +1,7 @@
+String name = 'name';
+String email = 'email';
+String mobile  = 'mobile';
+DateTime dob = dob;
+String gender = 'gender';
+List<String> hobbies = hobbies;
+String password = 'password';
