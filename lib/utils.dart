6e0e2f0fb0d2973020114c6b07f07df757f21e69
Manavkotecha <146665096+Manavkotecha@users.noreply.github.com>
2025-02-18 01:34:54 +0530

    Delete lib directory

--- lib/utils.dart
+++
@@ -1,7 +0,0 @@
-String name = 'name';
-String email = 'email';
-String mobile  = 'mobile';
-DateTime dob = dob;
-String gender = 'gender';
-List<String> hobbies = hobbies;
-String password = 'password';
